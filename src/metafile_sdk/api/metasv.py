942760928926bf6ca3d3e2ccb4fd37d843185464
xiangpeng <xiangpeng_work@126.com>
2021-11-01 15:43:06 +0800

    Add: 添加sensible query, metasv api支持

---
+++ src/metafile_sdk/api/metasv.py
@@ -0,0 +1,50 @@
+import json
+
+from bitsv.network.meta import Unspent
+from bitsv.transaction import calc_txid
+from metafile_sdk.api.api_base import ApiBase
+
+
+class MetasvApi(ApiBase):
+    # 获取utxo
+    _address_utxo = '/address/{}/utxo'
+    # 广播消息
+    _pushtx = '/tx/broadcast'
+
+    def __init__(self, metasv_api_key, base_url='https://apiv2.metasv.com', headers=None):
+        super(MetasvApi, self).__init__(base_url, headers)
+        self._headers = {
+            'Authorization': f'Bearer {metasv_api_key}'
+        }
+        if headers is not None:
+            self._headers.update(headers)
+        self.base_url = base_url
+
+    def get_unspents(self, address):
+        # 获取utxo
+        path = self._address_utxo.format(address)
+        utxo_list = self._get(path)
+        result_list = []
+        for utxo in utxo_list:
+            new_utxo = Unspent(amount=utxo['value'],
+                               confirmations=0,
+                               txid=utxo['txid'],
+                               txindex=utxo['outIndex'])
+            result_list.append(new_utxo)
+        return result_list
+
+    def broadcast_rawtx(self, rawtx):
+        path = self._pushtx
+        data = {
+            'hex': rawtx
+        }
+        resp = self._post(path, data, headers={
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        })
+        if resp['txid']:
+            return calc_txid(rawtx)
+        else:
+            message = resp['message']
+            raise ValueError(message)
+
---
+++ src/metafile_sdk/api/sensible_query.py
@@ -0,0 +1,57 @@
+from bitsv.network.meta import Unspent
+from bitsv.transaction import calc_txid
+from metafile_sdk.api.api_base import ApiBase
+
+
+class SensibleQueryApi(ApiBase):
+    # 获取utxo
+    _address_utxo = '/address/{}/utxo'
+    # 广播消息
+    _pushtx = '/pushtx'
+
+    def __init__(self, base_url='https://api.sensiblequery.com', headers=None):
+        super(SensibleQueryApi, self).__init__(base_url, headers)
+        self._headers = {}
+        if headers is not None:
+            self._headers.update(headers)
+        self.base_url = base_url
+
+    def get_unspents(self, address):
+        # 获取utxo
+        utxo_list = []
+        path = self._address_utxo.format(address)
+        buffer_size = 100
+        start = 0
+        while True:
+            sub_utxo_resp = self._get(path, params={
+                'cursor': start,
+                'size': buffer_size
+            })
+            sub_utxo = sub_utxo_resp['data']
+            utxo_list.extend(sub_utxo)
+            if len(sub_utxo) <= buffer_size:
+                break
+            start += buffer_size
+        result_list = []
+        for utxo in utxo_list:
+            new_utxo = Unspent(amount=utxo['satoshi'],
+                               confirmations=0,
+                               txid=utxo['txid'],
+                               txindex=utxo['vout'])
+            result_list.append(new_utxo)
+        return result_list
+
+    def broadcast_rawtx(self, rawtx):
+        path = self._pushtx
+        data = {
+            'txHex': rawtx
+        }
+        resp = self._post(path, data, headers={
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        })
+        code = resp['code']
+        if code == 0:
+            return calc_txid(rawtx)
+        else:
+            raise ValueError(resp.json())
